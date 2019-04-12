db474590f61ebcd7c4c3cdfd7821c976b2d57573
David Pye <david.pye@digital.cabinet-office.gov.uk>
2019-04-12 17:42:16 +0100

    add comentary to fetch.go

--- chap-1/1.5_Fetching_a_URL/fetch.go
+++ chap-1/1.5_Fetching_a_URL/fetch.go
@@ -9,18 +9,20 @@ import (
 )
 
 func main() {
-	for _, url := range os.Args[1:] {
-		resp, err := http.Get(url)
+	for _, url := range os.Args[1:] { // single cmdline arg please
+		resp, err := http.Get(url) // response or error returned
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err) // v=default fmt
 			os.Exit(1)
 		}
-		b, err := ioutil.ReadAll(resp.Body)
+		println("resp: ", resp, "err: ", err)
+		b, err := ioutil.ReadAll(resp.Body) // resp.Body contains server reply
+		// b contains server response now, next must close resp function
 		resp.Body.Close()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
+		fmt.Printf("%s", b) // s = uninterpreted string
 	}
 }

8fc9c7ab2d2291a8ee0c4081b4d10545496d9115
David Pye <david.pye@digital.cabinet-office.gov.uk>
2019-05-17 15:56:06 +0100

    add new version of webserver 3

--- chap-1/1.7_Web_Server/webserver-3-p22.go
+++ chap-1/1.7_Web_Server/webserver-3-p22.go
@@ -1,4 +1,4 @@
-// Server2 is a minimal "echo" and counter server.
+// Server3 is a minimal "echo" and counter server - improved  with p21 additions.
 package main
 
 import (
@@ -6,24 +6,36 @@ import (
 	"log"
 	"net/http"
 	"sync"
+
+	"github.com/alphagov/tlwr-dpye-ld/chap-1/lissajous"
 )
 
 var mu sync.Mutex
 var count int
 
 func main() {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		lissajous.Lissajous(w)
+	}
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/count", counter)
-	http.HandleFunc("/", lissa)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
-// handler echoes the Path component of the requested URL.
+// handler echoes the HTTP request.
 func handler(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	count++
-	mu.Unlock()
-	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
 }
 
 // counter echoes the number of calls so far.
@@ -33,6 +45,9 @@ func counter(w http.ResponseWriter, r *http.Request) {
 	mu.Unlock()
 }
 
-func lissa(w http.ResponseWriter, r *http.Request) {
-	lissajous(w)
-}
+// http.HandleFunc("/", handler)
+
+// func lissa(w http.ResponseWriter, r *http.Request) {
+// 	lissajous(w)
+// }
+// http.HandleFunc("/", lissa)
